385b39c88c8e81cca03a4b9ee822254a7e6eb2fd
paultheillustrator <info@paultheillustrator.com>
2014-06-01 23:04:27 +0200

    Fixes
    * Ratios to project thumbnails added
    * Fixed image son iPad fixed
    * General padding on Showreel Vimeo player improved

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,11 @@
 /* Show / hide About page JS */
 			$( ".about-link").click(function() {
-			  $(".about").removeClass( "fadeOut" ).toggleClass( "show fadeIn" );
+			  $(".about").removeClass( "hide fadeOut" ).toggleClass( "show fadeIn" );
 			});
 			$( ".about").click(function() {
-			  $(".about").removeClass( "show fadeIn" ).toggleClass( "fadeOut" );
+			  $(".about").removeClass( "show fadeIn" ).toggleClass( "hide fadeOut" );
 			});
 			/* Animate cross 
 			$( ".about-link").click(function() {
 			  $(".cross").removeClass( "rotate" ).toggleClass( "rotate" );
 			});*/
-
-
-
